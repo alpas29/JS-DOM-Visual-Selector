4bf2d820b84db1de60a6d3f40eca66207ac4be72
Christopher Doyle <christopher.odoyle@gmail.com>
2014-10-06 10:46:34 +0100

    [MOD] merged working into master

--- jsdom.js
+++ jsdom.js
@@ -1,129 +1,146 @@
 // vim: ts=2:sw=2
 
-var __inspectorPlugin = (function() {
+var __inspectorPlugin = __inspectorPlugin || {};
 
-    /* ======= Element Info Functions
-     */
-    var boxElement = document.createElement('div');
+var __inspectorPlugin = {
 
-    // LOCAL FUNCTIONS
-    function getElInfo(e) {
-      name = "&lt;" + e.tagName.toLowerCase() + "&gt; " + e.clientWidth + "x" + e.clientHeight;
-      return name;
-    }
-    function showInfoBox(infoData,e) {
-      rect = e.getBoundingClientRect();
-      cssStyleString = "position: absolute; top: "+(rect.top-18)+"px; left: "+(rect.left+5)+"px; line-height: 12px; font-size: 12px; padding: 1px; border: 1px solid black; background-color: #ADD8E6;";
-      boxElement.style.cssText = cssStyleString;
-      boxElement.innerHTML = infoData;
-      document.body.appendChild(boxElement);
-    }
+  INIT: function() {       // initialization for inspector start
+    boxElement = document.createElement('div');
+    divTop = document.createElement('div');
+    divBottom = document.createElement('div');
+    divLeft = document.createElement('div');
+    divRight = document.createElement('div');
+    this.addInspectorButtons();
+  },
+
+  /* ======= Element Info 
+   */
+
+  elementInfo: function(e) {
+    infoData = this.getElInfo(e);
+    this.showInfoBox(infoData,e);
+  },
+  getElInfo: function(e) {
+    name = "&lt;" + e.tagName.toLowerCase() + "&gt; " + e.clientWidth + "x" + e.clientHeight;
+    return name;
+  },
+  showInfoBox: function (infoData,e) {
+    rect = e.getBoundingClientRect();
+    cssStyleString = "position: absolute; top: "+(rect.top-18)+"px; left: "+(rect.left+5)+"px; line-height: 12px; font-size: 12px; padding: 1px; border: 1px solid black; background-color: #ADD8E6;";
+    boxElement.style.cssText = cssStyleString;
+    boxElement.innerHTML = infoData;
+    document.body.appendChild(boxElement);
+  },
 
     /* ======== Adding/Removing Inspector Box
      */
 
-    function addEdgeDiv(newEl, width, height, ptop,pleft) {
-      newEl.style.position = "absolute";
-      newEl.style.top = ptop+"px";
-      newEl.style.left = pleft+"px";
-      newEl.style.height = height+"px";
-      newEl.style.width = width+"px";
-      newEl.style.background = "#FF69B4";
-      document.body.appendChild(newEl);
+  INSBOXFUN: {
+    INIT: function() {
+      var kapa = __inspectorPlugin;
+      kapa.addEvent(document.body, "mouseover", kapa.mouseOverHandler);
+      kapa.addEvent(document.body, "mouseout", kapa.mouseOutHandler);
+      kapa.addEvent(document.body, "mousedown", kapa.mouseDownHandler);
+    },
+    EXIT: function() {
+      var kapa = __inspectorPlugin;
+      kapa.removeEvent(document.body, "mouseover", kapa.mouseOverHandler);
+      kapa.removeEvent(document.body, "mousedown", kapa.mouseDownHandler);
+      kapa.removeEvent(document.body, "mouseout", kapa.mouseOutHandler); // remove kapa so that highlighting remains 
     }
+  },
 
-    /* ======== Mouse Event Handlers 
-     */
-    function mouseOverHandler(m) {
-      if(m.target.id !== "INSPECTORBUTTON000") {
-        drawBoundingBox(m.target);
-      }
-      if(m.target.id !== "INSPECTORBUTTON000") {
-        elementInfo(m.target);
-      }
+  mouseOverHandler: function(m) {
+    if(m.target.id !== "INSPECTORBUTTON000") {
+      __inspectorPlugin.drawBoundingBox(m.target);
+      __inspectorPlugin.elementInfo(m.target);
     }
-    function mouseOutHandler(m) {
-      removeBoundingBox();
+  },
+  mouseOutHandler: function(m) {
+    __inspectorPlugin.removeBoundingBox();
+  },
+  mouseDownHandler: function(m) {
+    __inspectorPlugin.pauseInspector();
+  },
+  addEvent: function(element, eventName, handler) {
+    if (element.addEventListener) {
+      element.addEventListener(eventName, handler, false);
+    } else if (element.attachEvent) {
+      element.attachEvent("on" + eventName, handler);
     }
-    function mouseDownHandler(m) {
-      __inspectorPlugin.pauseInspector();
+  },
+  removeEvent: function(element, eventName, handler) {
+    if (element.removeEventListener) {
+      element.removeEventListener(eventName, handler, false);
+    } else if (element.removeEvent) {
+      element.removeEvent("on" + eventName, handler);
     }
+  },
+  addEdgeDiv: function(newEl, width, height, ptop, pleft) {
+    newEl.style.position = "absolute";
+    newEl.style.top = ptop+"px";
+    newEl.style.left = pleft+"px";
+    newEl.style.height = height+"px";
+    newEl.style.width = width+"px";
+    newEl.style.background = "#FF69B4";
+    document.body.appendChild(newEl);
+  },
+  drawBoundingBox: function(chosenEl) {
+    // borders/outlines/margins/padding etc. will effect page layout
+    // instead, add 4 divs positioned atop the element
+    var box = chosenEl.getBoundingClientRect();
+    var xOffset = window.scrollX;
+    var yOffset = window.scrollY;
 
-    // Cross-bOWSer event listener functions
-    function addEvent(element, eventName, handler) {
-      if (element.addEventListener) {
-        element.addEventListener(eventName, handler, false);
-      } else if (element.attachEvent) {
-        element.attachEvent("on" + eventName, handler);
-      }
-    }
-    function removeEvent(element, eventName, handler) {
-      if (element.removeEventListener) {
-        element.removeEventListener(eventName, handler, false);
-      } else if (element.removeEvent) {
-        element.removeEvent("on" + eventName, handler);
-      }
-    }
+    this.addEdgeDiv(divTop, box.width, 2, box.top+yOffset, box.left+xOffset);
+    this.addEdgeDiv(divBottom, box.width, 2, box.bottom+yOffset, box.left+xOffset);
+    this.addEdgeDiv(divLeft, 2, box.height, box.top+yOffset, box.left+xOffset);
+    this.addEdgeDiv(divRight, 2, box.height, box.top+yOffset, box.right+xOffset);
+  },
+  removeBoundingBox: function() {
+    document.body.removeChild(divTop);
+    document.body.removeChild(divBottom);
+    document.body.removeChild(divLeft);
+    document.body.removeChild(divRight);
+  },
 
-    function elementInfo(e) {
-        infoData = getElInfo(e);
-        showInfoBox(infoData,e);
-      }
-    function drawBoundingBox(chosenEl) {
-      // borders/outlines/margins/padding etc. will effect page layout
-      // instead, add 4 divs positioned atop the element
-      var box = chosenEl.getBoundingClientRect();
+  startInspector: function() {
+    __inspectorPlugin.INSBOXFUN.INIT();
+    document.body.style.cursor = 'crosshair';
+    startBtn.innerHTML = "Pause Inspector";
+    startBtn.setAttribute("onclick", "__inspectorPlugin.pauseInspector()");
+  },
+  stopInspector: function() {
+    __inspectorPlugin.pauseInspector();
+    document.body.removeChild(boxElement);
+  },
+  pauseInspector: function() {
+    document.body.style.cursor = 'auto';
+    startBtn.innerHTML = "Start Inspector";
+    startBtn.setAttribute("onclick", "__inspectorPlugin.startInspector()");
+    __inspectorPlugin.INSBOXFUN.EXIT();
+  },
+  addInspectorButtons: function() {
+    startBtn = document.createElement("button");
+    startBtn.setAttribute("type", "button");
+    startBtn.setAttribute("onclick", "__inspectorPlugin.startInspector()");
+    startBtn.setAttribute("class", "INSPECTORBUTTON000");
+    startBtn.style.cssText = 'position: absolute; top: 0; right: 0;';
+    startBtn.innerHTML = "Start Inspector";
 
-      divTop = document.createElement('div');
-      divBottom = document.createElement('div');
-      divLeft = document.createElement('div');
-      divRight = document.createElement('div');
+    stopBtn = document.createElement("button");
+    stopBtn.setAttribute("type", "button");
+    stopBtn.setAttribute("onclick", "__inspectorPlugin.stopInspector()");
+    stopBtn.setAttribute("class", "INSPECTORBUTTON000");
+    stopBtn.style.cssText = 'position: absolute; top: 25px; right: 0;';
+    stopBtn.innerHTML = "Stop Inspector";
 
-      var xOffset = window.scrollX;
-      var yOffset = window.scrollY;
+    document.body.appendChild(stopBtn);
+    document.body.appendChild(startBtn);
+  }
+}
 
-      addEdgeDiv(divTop, box.width, 2, box.top+yOffset, box.left+xOffset);
-      addEdgeDiv(divBottom, box.width, 2, box.bottom+yOffset, box.left+xOffset);
-      addEdgeDiv(divLeft, 2, box.height, box.top+yOffset, box.left+xOffset);
-      addEdgeDiv(divRight, 2, box.height, box.top+yOffset, box.right+xOffset);
-    }
-      
-    function removeBoundingBox() {
-      document.body.removeChild(divTop);
-      document.body.removeChild(divBottom);
-      document.body.removeChild(divLeft);
-      document.body.removeChild(divRight);
-    }
-
-    return {
-      startInspector: function() {
-        addEvent(document.body, "mouseover", mouseOverHandler);
-        addEvent(document.body, "mouseout", mouseOutHandler);
-        addEvent(document.body, "mousedown", mouseDownHandler);
-        btn.innerHTML = "Pause Inspector";
-        btn.setAttribute("onclick", "__inspectorPlugin.pauseInspector()");
-        btn.parentNode.replaceChild(btn,btn);
-      },
-      pauseInspector: function() {
-        removeEvent(document.body, "mouseover", mouseOverHandler);
-        removeEvent(document.body, "mousedown", mouseDownHandler);
-        removeEvent(document.body, "mouseout", mouseOutHandler); // remove this so that highlighting remains
-        btn.innerHTML = "Start Inspector";
-        btn.setAttribute("onclick", "__inspectorPlugin.startInspector()");
-      },
-      addInspectorButton: function() {
-        btn = document.createElement("button");
-        btn.setAttribute("type", "button");
-        btn.setAttribute("onclick", "__inspectorPlugin.startInspector()");
-        btn.setAttribute("id", "INSPECTORBUTTON000");
-        btn.style.cssText = 'position: absolute; top: 0; right: 0;';
-        btn.innerHTML = "Start Inspector";
-        document.body.appendChild(btn);    
-      }
-    }
-})();
-
-__inspectorPlugin.addInspectorButton();
+__inspectorPlugin.INIT();
 
 
 
